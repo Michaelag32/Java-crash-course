9bf3a18746cbb741e4f01f5ef15d79ebbdb885cf
Michaelag32 <Mikelagg32@gmail.com>
2022-08-16 02:12:07 -0700

    reviewed functions

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
 console.log(convertCelsiusToFahrenheit (0))*/
 
 
-for (let i=1; i<=20; i++) {
+/*for (let i=1; i<=20; i++) {
 
 if (i % 3 === 0) {
     console.log(`${i} --> Frontend`)
@@ -16,6 +16,14 @@ if (i % 5 === 0)
 console.log(`${i} --> Simplified`)
 
 else {
-    console.log('Frontend Simplified ')
+    console.log('Frontend Simplified ')*/
+
+
+const convertCelsiusToFahrenheit = (celsius) => {
+    return celsius * 1.8 + 32
+
 }
-}
+
+console.log(convertCelsiusToFahrenheit (0))
+
+

78d4118379a73581898194c2f74545d4fca1d3e1
Michaelag32 <Mikelagg32@gmail.com>
2022-09-07 11:37:34 -0700

    java practice

--- app.js
+++ app.js
@@ -19,11 +19,60 @@ else {
     console.log('Frontend Simplified ')*/
 
 
-const convertCelsiusToFahrenheit = (celsius) => {
+/*const convertCelsiusToFahrenheit = (celsius) => {
     return celsius * 1.8 + 32
 
 }
 
-console.log(convertCelsiusToFahrenheit (0))
+console.log(convertCelsiusToFahrenheit (0))*/ 
 
+/* let arr = [20,30,40,50,100]
+
+let newArr = arr.filter ((element) => {
+console.log(element)
+if (element < 50) {
+
+    return true;
+}
+})
+
+console.log(newArr) */ 
+
+
+/*let grades = ['A+', 'A', 'FAIL']
+
+let goodgrades = grades.filter ((element) => {
+    console.log (element)
+    if (element !== 'FAIL') {
+        return true
+    }
+})
+
+console.log (goodgrades) -----> Checkedddddd*/
+
+/*let grades = ['A+', 'A', 'FAIL']
+
+let goodgrades = []
+
+for(let i = 0; i < grades.length; i++) {
+    if (grades[i] !== 'FAIL')
+    goodgrades.push(grades [i])
+}
+console.log (goodgrades) -------> checkedddddd*/
+
+/*let dollars = [1, 5, 10, 3]
+
+let cents = dollars.map (dollars => dollars * 100) 
+
+console.log (cents) ---------- short version checkeddddd */
+
+
+/* let dollars = [0 , 10, 20] 
+
+let cents = dollars.map ((element) => {
+    console.log (element)
+    return element * 100
+})
+
+console.log (cents) --------------> long version checked */ 
 
